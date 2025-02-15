b1340eeb3fd88c1f9b4c8f61e616c4f79ae01b87
Julia Solias <juliasohu@gmail.com>
2025-02-15 11:09:24 +0100

    Add link to go to Category page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,9 @@
+import { useNavigate } from "react-router-dom";
+
 //TODO Tailwind classes not applying correctly
 function Homepage() {
+  const navigate = useNavigate();
+
   return (
     <div className="bg-gradient-to-r from-pastelPink via-pastelBlue to-pastelGreen min-h-screen flex flex-col justify-center items-center text-center p-6">
       <div className="bg-white bg-opacity-90 p-6 rounded-xl shadow-xl border-4 border-pastelYellow">
@@ -13,8 +17,8 @@ function Homepage() {
         <p className="text-gray-700 mb-6">
           A place to collect thoughts, memories, and messages from friends. Join in and leave your mark!
         </p>
-        <button className="bg-pastelBlue hover:bg-vibrantPurple text-white font-bold py-2 px-6 rounded-full transition duration-300">
-          View Entries
+        <button onClick={() => navigate('/category')} className="bg-pastelBlue hover:bg-vibrantPurple font-bold py-2 px-6 rounded-full transition duration-300">
+          View Teams 
         </button>
       </div>
     </div>

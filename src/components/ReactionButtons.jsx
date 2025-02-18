47fd9b81dfeaf1199956e8b1d398c4cfde5e07d0
Julia Solias <juliasohu@gmail.com>
2025-02-18 16:30:12 +0100

    Updated icons and counter

--- src/components/ReactionButtons.jsx
+++ src/components/ReactionButtons.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 function ReactionButtons() {
   const [counts, setCounts] = useState({
-    like: 0,
+    heart: 0,
     cheer: 0,
     celebrate: 0,
     appreciate: 0,
@@ -18,20 +18,20 @@ function ReactionButtons() {
   };
 
   const reactions = [
-    { emoji: '👍', label: 'Like', type: 'like' },
     { emoji: '👏🏻', label: 'Cheer', type: 'cheer' },
+    { emoji: '🩷', label: 'Heart', type: 'heart' },
     { emoji: '🎉', label: 'Celebrate', type: 'celebrate' },
-    { emoji: '✨', label: 'Appreciate', type: 'appreciate' },
-    { emoji: '🙂', label: 'Smile', type: 'smile' },
+    { emoji: '😊', label: 'Smile', type: 'smile' },
+    { emoji: '✨', label: 'Appreciate', type: 'appreciate' }
   ];
 
   return (
-    <div className="hover:scale-x-105 transition-all duration-300 flex justify-start text-2xl items-center shadow-xl z-10 bg-white dark:bg-white gap-2 p-2 rounded-full">
+    <div className="flex justify-start text-2xl items-center shadow-xl z-10 bg-white dark:bg-white gap-2 p-2 rounded-full">
       {reactions.map((reaction, index) => (
         <button
           key={index}
           onClick={() => handleReaction(reaction.type)}
-          className="before:hidden hover:before:flex before:justify-center before:items-center before:h-4 before:text-[.6rem] before:px-1 before:content-[attr(data-label)] before:bg-gray-800 before:text-white before:bg-opacity-50 before:absolute before:-top-7 before:rounded-lg hover:-translate-y-5 cursor-pointer hover:scale-125 bg-white rounded-full p-2 px-3"
+          className="before:hidden hover:before:flex before:justify-center before:items-center before:h-4 before:text-[.6rem] before:px-1 before:content-[attr(data-label)] before:bg-gray-800 before:text-white before:bg-opacity-50 before:absolute before:-top-6 before:rounded-lg hover:-translate-y-2 cursor-pointer hover:scale-110 bg-white rounded-full p-2 px-3"
           data-label={reaction.label}
         >
           {reaction.emoji} {counts[reaction.type]}

fb28398ba9e4a9f5d4790aebe1c95f0a4c58752d
Julia Solias <juliasohu@gmail.com>
2025-02-19 17:54:11 +0100

    Erased footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import MembersPage from "./pages/MembersPage";
 import Profile from "./pages/ProfileDetailPage";
 import ErrorPage from "./pages/ErrorPage";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import CreateProfilePage from "./pages/CreateProfilePage";
 import About from "./pages/AboutPage";
 import EditDetailsPage from "./pages/EditDetailsPage";
--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <div>
-      <p>git repo</p>
-    </div>
-  );
-}
-export default Footer;

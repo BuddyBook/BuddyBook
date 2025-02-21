3e520f5ea766b205145c78b8a6ae8d45216a0307
Julia Solias <juliasohu@gmail.com>
2025-02-21 09:24:53 +0100

    Typo message access

--- src/pages/TeamsPage.jsx
+++ src/pages/TeamsPage.jsx
@@ -288,8 +288,8 @@ function TeamsPage() {
                       required
                     />
                     <small className="text-red-800 font-bold">
-                      Please copy and share with people who want to join your
-                      team. This is one time code do not lose it
+                      Please copy and share only with the people in your
+                      team. This is a one-time code, do not lose it.
                     </small>
                   </div>
                 )}
